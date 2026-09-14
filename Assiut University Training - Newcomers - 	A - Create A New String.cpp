5cbdbe129d3ed6ec945820d90a6a671073d0333c
HGGMco15 <ht900193@gmail.com>
2026-09-14 18:19:21 +0700

    Create Assiut University Training - Newcomers - 	A - Create A New String.cpp

---
+++ Assiut University Training - Newcomers - 	A - Create A New String.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    string s,t;
+    cin>>s;
+    cin>>t;
+    long long i1=s.length(),i2=t.length();
+    cout<<i1<<" "<<i2<<"\n";
+    cout<<s<<" "<<t<<"\n";
+}
